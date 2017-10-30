d73635e9271d0189df2f027a9d5556a20f84d265
Sarath S. Nair <saratnai@oradev.oraclecorp.com>
2017-10-30 18:50:21 +0530

    Initial Commit

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
+
+@Component({
+  styleUrls: ['./heroes.component.css'],
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+
+  selectedHero: Hero;
+  heroes: Hero[];
+
+
+  constructor(
+    private heroService: HeroService,
+    private router: Router) { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  gotoDetails(): void {
+    this.router.navigate(['/detail', this.selectedHero.id])
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h != hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      })
+  }
+}
